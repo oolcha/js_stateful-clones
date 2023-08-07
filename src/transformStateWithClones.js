a22e05bf237ebc17d2bb6885031c772e5dcab734
oolcha <olha.bondarenko.job@gmail.com>
2023-08-07 16:34:13 +0300

    initial commit

---
+++ src/transformStateWithClones.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @param {Object} state
+ * @param {Object[]} actions
+ *
+ * @return {Object[]}
+ */
+function transformStateWithClones(state, actions) {
+  // write code here
+}
+
+module.exports = transformStateWithClones;
